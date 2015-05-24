0ceadf2485bf43ebe2c038eb7f2610a6fd2ea0e4
AntoineLatendresse <a.latendresse2@hotmail.com>
2015-05-23 21:15:43 -0400

    first push
    ma essayer de faire le calendrier bientot

---
+++ MVC_TP_FINAL/Controllers/EventCalenderController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_TP_FINAL.Controllers
+{
+    public class EventCalenderController : Controller
+    {
+        //
+        // GET: /EventCalender/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+	}
+}
